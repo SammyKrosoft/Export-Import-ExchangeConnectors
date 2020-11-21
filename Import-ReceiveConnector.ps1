e426534cfe6e829cd9a500cd15b63a38c4dd58a1
Sammy Krosoft <sammy@hotmail.fr>
2020-11-21 01:28:34 -0500

    update

--- Import-ReceiveConnector.ps1
+++ Import-ReceiveConnector.ps1
@@ -20,69 +20,97 @@ Catch {
 
 
 Foreach ($MyConnector in $ReceiveConnectors){
+       Write-Host "Creating Connector $($MyConnector.Name) on server $Server" -BackgroundColor Yellow -ForegroundColor Blue
+       $Properties = @{    
+       AuthTarpitInterval = $MyConnector.AuthTarpitInterval
+       TarpitInterval = $MyConnector.TarpitInterval
+       MaxAcknowledgementDelay = $MyConnector.MaxAcknowledgementDelay
+       ConnectionInactivityTimeout = $MyConnector.ConnectionInactivityTimeout
+       ConnectionTimeout = $MyConnector.ConnectionTimeout
+       MaxInboundConnectionPercentagePerSource = $MyConnector.MaxInboundConnectionPercentagePerSource
+       MaxLogonFailures = $MyConnector.MaxLogonFailures
+       MaxProtocolErrors = $MyConnector.MaxProtocolErrors
+       MaxLocalHopCount = $MyConnector.MaxLocalHopCount
+       MaxInboundConnectionPerSource = $MyConnector.MaxInboundConnectionPerSource
+       MaxHopCount = $MyConnector.MaxHopCount
+       MaxRecipientsPerMessage = $MyConnector.MaxRecipientsPerMessage
+       MaxInboundConnection = $MyConnector.MaxInboundConnection
+       Name = $MyConnector.Name
+ 
+       #DistinguishedName = $MyConnector.DistinguishedName #Parameter set automatically (combination of name, server, domain, ...)
+       Fqdn = $MyConnector.Fqdn
+       SizeEnabled = $MyConnector.SizeEnabled
+       TransportRole = $MyConnector.TransportRole
+       MessageRateSource = $MyConnector.MessageRateSource
+       ExtendedProtectionPolicy = $MyConnector.ExtendedProtectionPolicy
+       ProtocolLoggingLevel = $MyConnector.ProtocolLoggingLevel
+       AuthMechanism = $MyConnector.AuthMechanism
+       MessageRateLimit = $MyConnector.MessageRateLimit
+       #AcceptConsumerMail =  [System.Convert]::ToBoolean($($MyConnector.AcceptConsumerMail)) #Parameter reserved for internal Microsoft use
+       AdvertiseClientSettings =  [System.Convert]::ToBoolean($($MyConnector.AdvertiseClientSettings))
+       #BareLinefeedRejectionEnabled =  [System.Convert]::ToBoolean($($MyConnector.BareLinefeedRejectionEnabled)) #Parameter only available with Set-ReceiveConnector
+       DomainSecureEnabled =  [System.Convert]::ToBoolean($($MyConnector.DomainSecureEnabled))
+       EnableAuthGSSAPI =  [System.Convert]::ToBoolean($($MyConnector.EnableAuthGSSAPI))
+       #LiveCredentialEnabled =  [System.Convert]::ToBoolean($($MyConnector.LiveCredentialEnabled)) #Parameter reserved for internal Microsoft use
+       LongAddressesEnabled =  [System.Convert]::ToBoolean($($MyConnector.LongAddressesEnabled))
+       OrarEnabled =  [System.Convert]::ToBoolean($($MyConnector.OrarEnabled))
+       #ProxyEnabled =  [System.Convert]::ToBoolean($($MyConnector.ProxyEnabled)) #Parameter reserved for internal Microsoft use
+       RejectReservedSecondLevelRecipientDomains =  [System.Convert]::ToBoolean($($MyConnector.RejectReservedSecondLevelRecipientDomains))
+       RejectReservedTopLevelRecipientDomains =  [System.Convert]::ToBoolean($($MyConnector.RejectReservedTopLevelRecipientDomains))
+       RejectSingleLabelRecipientDomains =  [System.Convert]::ToBoolean($($MyConnector.RejectSingleLabelRecipientDomains))
+       RequireEHLODomain =  [System.Convert]::ToBoolean($($MyConnector.RequireEHLODomain))
+       RequireTLS =  [System.Convert]::ToBoolean($($MyConnector.RequireTLS))
+       SuppressXAnonymousTls =  [System.Convert]::ToBoolean($($MyConnector.SuppressXAnonymousTls))
+       BinaryMimeEnabled =  [System.Convert]::ToBoolean($($MyConnector.BinaryMimeEnabled))
+       ChunkingEnabled =  [System.Convert]::ToBoolean($($MyConnector.ChunkingEnabled))
+       DeliveryStatusNotificationEnabled =  [System.Convert]::ToBoolean($($MyConnector.DeliveryStatusNotificationEnabled))
+       EightBitMimeEnabled =  [System.Convert]::ToBoolean($($MyConnector.EightBitMimeEnabled))
+       Enabled =  [System.Convert]::ToBoolean($($MyConnector.Enabled))
+       EnhancedStatusCodesEnabled =  [System.Convert]::ToBoolean($($MyConnector.EnhancedStatusCodesEnabled))
+       PipeliningEnabled =  [System.Convert]::ToBoolean($($MyConnector.PipeliningEnabled))
+       #SmtpUtf8Enabled =  [System.Convert]::ToBoolean($($MyConnector.SmtpUtf8Enabled)) #Parameter reserved for internal Microsoft use
+       MaxHeaderSize = $($MyConnector.MaxHeaderSize) + "KB"
+       MaxMessageSize = $($MyConnector.MaxMessageSize) + "MB"
+       
+}
 
-    $Properties = @{    
-                        Server = $Server              
-                        AcceptConsumerMail = $MyConnector.AcceptConsumerMail
-                        AdvertiseClientSettings = $MyConnector.AdvertiseClientSettings
-                        AuthMechanism = $MyConnector.AuthMechanism
-                        AuthTarpitInterval = $MyConnector.AuthTarpitInterval
-                        Banner = $MyConnector.Banner
-                        BareLinefeedRejectionEnabled = $MyConnector.BareLinefeedRejectionEnabled
-                        BinaryMimeEnabled = $MyConnector.BinaryMimeEnabled
-                        Bindings = ($MyConnector.Bindings -split ";")
-                        ChunkingEnabled = $MyConnector.ChunkingEnabled
-                        Comment = $MyConnector.Comment
-                        ConnectionInactivityTimeout = $MyConnector.ConnectionInactivityTimeout
-                        ConnectionTimeout = $MyConnector.ConnectionTimeout
-                        DefaultDomain = $MyConnector.DefaultDomain
-                        DeliveryStatusNotificationEnabled = $MyConnector.DeliveryStatusNotificationEnabled
-                        DistinguishedName = $MyConnector.DistinguishedName
-                        DomainSecureEnabled = $MyConnector.DomainSecureEnabled
-                        EightBitMimeEnabled = $MyConnector.EightBitMimeEnabled
-                        EnableAuthGSSAPI = $MyConnector.EnableAuthGSSAPI
-                        Enabled = $MyConnector.Enabled
-                        EnhancedStatusCodesEnabled = $MyConnector.EnhancedStatusCodesEnabled
-                        ExtendedProtectionPolicy = $MyConnector.ExtendedProtectionPolicy
-                        Fqdn = $MyConnector.Fqdn
-                        LiveCredentialEnabled = $MyConnector.LiveCredentialEnabled
-                        LongAddressesEnabled = $MyConnector.LongAddressesEnabled
-                        MaxAcknowledgementDelay = $MyConnector.MaxAcknowledgementDelay
-                        MaxHeaderSize = $($MyConnector.MaxHeaderSize/1KB)
-                        MaxHopCount = $MyConnector.MaxHopCount
-                        MaxInboundConnection = $MyConnector.MaxInboundConnection
-                        MaxInboundConnectionPercentagePerSource = $MyConnector.MaxInboundConnectionPercentagePerSource
-                        MaxInboundConnectionPerSource = $MyConnector.MaxInboundConnectionPerSource
-                        MaxLocalHopCount = $MyConnector.MaxLocalHopCount
-                        MaxLogonFailures = $MyConnector.MaxLogonFailures
-                        MaxMessageSize = $($MyConnector.MaxMessageSize/1MB)
-                        MaxProtocolErrors = $MyConnector.MaxProtocolErrors
-                        MaxRecipientsPerMessage = $MyConnector.MaxRecipientsPerMessage
-                        MessageRateLimit = $MyConnector.MessageRateLimit
-                        MessageRateSource = $MyConnector.MessageRateSource
-                        Name = $MyConnector.Name
-                        OrarEnabled = $MyConnector.OrarEnabled
-                        PermissionGroups = $MyConnector.PermissionGroups
-                        PipeliningEnabled = $MyConnector.PipeliningEnabled
-                        ProtocolLoggingLevel = $MyConnector.ProtocolLoggingLevel
-                        ProxyEnabled = $MyConnector.ProxyEnabled
-                        RejectReservedSecondLevelRecipientDomains = $MyConnector.RejectReservedSecondLevelRecipientDomains
-                        RejectReservedTopLevelRecipientDomains = $MyConnector.RejectReservedTopLevelRecipientDomains
-                        RejectSingleLabelRecipientDomains = $MyConnector.RejectSingleLabelRecipientDomains
-                        RemoteIPRanges = ($MyConnector.RemoteIPRanges -join ";")
-                        RequireEHLODomain = $MyConnector.RequireEHLODomain
-                        RequireTLS = $MyConnector.RequireTLS
-                        ServiceDiscoveryFqdn = $MyConnector.ServiceDiscoveryFqdn
-                        SizeEnabled = $MyConnector.SizeEnabled
-                        SmtpUtf8Enabled = $MyConnector.SmtpUtf8Enabled
-                        SuppressXAnonymousTls = $MyConnector.SuppressXAnonymousTls
-                        TarpitInterval = $MyConnector.TarpitInterval
-                        TlsCertificateName = $MyConnector.TlsCertificateName
-                        TlsDomainCapabilities = ($MyConnector.TlsDomainCapabilities -join ";")
-                        TransportRole = $MyConnector.TransportRole
-    }
+IF(-not [string]::IsNullOrEmpty($($MyConnector.ServiceDiscoveryFqdn))){$properties.Add("ServiceDiscoveryFqdn",$($MyConnector.ServiceDiscoveryFqdn))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.TlsDomainCapabilities))){$properties.Add("TlsDomainCapabilities" , $($MyConnector.TlsDomainCapabilities -join ";"))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.Bindings))){$properties.Add("Bindings",$($MyConnector.Bindings -join ";"))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.RemoteIPRanges))){$properties.Add("RemoteIPRanges",$($MyConnector.RemoteIPRanges -join ";"))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.Banner))){$properties.Add("Banner",$($MyConnector.Banner))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.Comment))){$properties.Add("Comment",$($MyConnector.Comment))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.DefaultDomain))){$properties.Add("DefaultDomain",$($MyConnector.DefaultDomain))}
+IF(-not [string]::IsNullOrEmpty($($MyConnector.TlsCertificateName))){$properties.Add("TlsCertificateName",$($MyConnector.TlsCertificateName))}
 
-New-ReceiveConnector @Properties
+#Bindings = ($MyConnector.Bindings -join ";")
+#RemoteIPRanges = ($MyConnector.RemoteIPRanges -join ";")
+#TlsDomainCapabilities = ($MyConnector.TlsDomainCapabilities -join ";")
+#Banner = $MyConnector.Banner
+# Comment = $MyConnector.Comment
+# DefaultDomain = $MyConnector.DefaultDomain
+#ServiceDiscoveryFqdn = $MyConnector.ServiceDiscoveryFqdn
+# TlsCertificateName = $MyConnector.TlsCertificateName
+
+If ($($MyConnector.PermissionGroups) -match 'Custom'){
+       Write-Host "Warning: This connector have AD Extended permissions like ms-Exch-SMTP-Accept-Any-Recipient or ms-Exch-SMTP-Accept-Any-Sender - please use Add-ADPermissions with the -ExtendedRights parameter" -ForegroundColor Yellow
+} Else {
+       IF(-not [string]::IsNullOrEmpty($($MyConnector.PermissionGroups))){$properties.Add("PermissionGroups",$($MyConnector.PermissionGroups))}
+}
+
+#PermissionGroups = $MyConnector.PermissionGroups
+
+
+#$Properties
+
+Try{
+       New-ReceiveConnector -Server $Server @Properties -ErrorAction Stop
+} catch {
+       Write-Host "Issue creating this connector - connector NOT created - See below error:" -ForegroundColor red
+       $ErrorMessage = $_.Exception.Message
+       Write-Host "$ErrorMessage" -ForegroundColor DarkMagenta
+}
+
 }
 
 Write-Host "Finished..." -ForegroundColor Green
